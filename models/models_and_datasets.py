86bcf19a24df5257ee87f5624909b8d1e028bdd4
10258392511 <44485479+10258392511@users.noreply.github.com>
2020-12-11 12:30:17 +0800

    Update models_and_datasets.py

--- models/models_and_datasets.py
+++ models/models_and_datasets.py
@@ -3,12 +3,38 @@ import matplotlib.pyplot as plt
 import cv2 as cv
 import torch
 import torch.nn as nn
+import os
+import torchvision
+import utils
+import transforms as T
+import random
+
 from PIL import Image
-# from CNN_mask_detect import *
-# models
+from torchvision import datasets
+from facenet_pytorch import MTCNN
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+from torch.nn import Softmax, Dropout, Linear
+from torchvision import models
+from torchvision.transforms import Compose, Resize, ToPILImage, ToTensor, CenterCrop, Normalize
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+
+
+def get_model_instance_segmentation(num_classes):
+  model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+
+  in_features = model.roi_heads.box_predictor.cls_score.in_features
+  model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+  in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+  hidden_layer = 256
+  model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask, hidden_layer, num_classes)
+
+  return model
+
 
 H_all, W_all = 248, 173
-
 class ImageEncoder(nn.Module):
   def __init__(self, H_all=H_all, W_all=W_all, num_res=4):
     super(ImageEncoder, self).__init__()
@@ -171,4 +197,106 @@ class MaskModel(nn.Module):
 
 #########################################################################################
 # datasets
+# reference: https://pytorch.org/tutorials/intermediate/torchvision_tutorial.html
+class PennFudanDataset(torch.utils.data.Dataset):
+  def __init__(self, root, transforms):
+    self.root = root
+    self.transforms = transforms
+    self.imgs = list(sorted(os.listdir(os.path.join(root, "PNGImages"))))
+    self.masks = list(sorted(os.listdir(os.path.join(root, "PedMasks"))))
+  
 
+  def __getitem__(self, idx):
+    img_path = os.path.join(self.root, "PNGImages", self.imgs[idx])
+    mask_path = os.path.join(self.root, "PedMasks", self.masks[idx])
+
+    img = Image.open(img_path).convert("RGB")
+    mask = Image.open(mask_path)
+    mask = np.array(mask)
+    obj_ids = np.unique(mask)
+    obj_ids = obj_ids[1:]
+
+    masks = (mask == obj_ids[:, None, None])
+    num_objs = len(obj_ids)
+
+    boxes = []
+    for i in range(num_objs):
+      pos = np.where(masks[i])
+      xmin = np.min(pos[1])
+      xmax = np.max(pos[1])
+      ymin = np.min(pos[0])
+      ymax = np.max(pos[0])
+      boxes.append([xmin, ymin, xmax, ymax])
+    
+    boxes = torch.as_tensor(boxes, dtype=torch.float32)
+    labels = torch.ones((num_objs,), dtype=torch.int64)
+    masks = torch.as_tensor(masks, dtype=torch.uint8)
+    image_id = torch.tensor([idx])
+    area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+    iscrowd = torch.zeros((num_objs, ), dtype=torch.int64)
+
+    target = {}
+    target["boxes"] = boxes
+    target["labels"] = labels
+    target["masks"] = masks
+    target["image_id"] = image_id
+    target["area"] = area
+    target["iscrowd"] = iscrowd
+
+    if self.transforms is not None:
+      img, target = self.transforms(img, target)  # NOTE: not from torchvision.transforms, 
+                                                  # but from another library
+    return img, target
+  
+  def __len__(self):
+    return len(self.imgs)
+
+  
+class FaceDataset(Dataset):
+  def __init__(self, imgs, masks, locs):
+    self.imgs = imgs.astype(np.float32)
+    self.masks = masks.astype(np.float32)
+    self.locs = locs.astype(np.float32)
+  
+  def __len__(self):
+    return self.imgs.shape[0]
+  
+  def __getitem__(self, idx):
+    # dict_out = {"mask": torch.tensor(self.masks[idx]), "locs": (self.locs[idx, 0], self.locs[idx, 1])}
+    # return torch.tensor(self.imgs[idx], dtype=torch.float32), torch.tensor(self.masks[idx], dtype=torch.float32), \
+    # (self.locs[idx, 0], self.locs[idx, 1])
+    return torch.tensor(self.imgs[idx]), torch.tensor(self.masks[idx]), \
+    (self.locs[idx, 0], self.locs[idx, 1])
+
+  
+# masked: 1, unmasked: 0
+class ResizedDataset(Dataset):
+  def __init__(self, masked_paths: list, unmasked_paths: list, tgt_size: tuple):
+    self.masked_paths = masked_paths
+    self.unmasked_paths = unmasked_paths
+    self.transform = T.Compose([
+      T.ToPILImage(),
+      T.ToTensor(),
+      T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+      T.Resize(tgt_size)
+    ])
+  
+  def __len__(self):
+    return len(self.masked_paths) + len(self.unmasked_paths)
+  
+  def __getitem__(self, idx):
+    num_masked, num_unmasked = len(self.masked_paths), len(self.unmasked_paths)
+    assert idx < (num_masked + num_unmasked) and idx >= 0, "invalid index"
+    path, label = None, -1
+    if idx >= num_masked:
+      idx -= num_masked
+      path = self.unmasked_paths[idx]
+      label = 0
+    else:
+      path = self.masked_paths[idx]
+      label = 1
+    
+    # img = (cv.cvtColor(cv.imread(path), cv.COLOR_BGR2RGB) / 255).astype(np.float32)
+    img = cv.cvtColor(cv.imread(path), cv.COLOR_BGR2RGB)
+
+    return self.transform(img), label
